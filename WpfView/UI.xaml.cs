3c19101d005748a6aae131508bebcc37db88586d
Johnny P <johnnnyppp@gmail.com>
2016-04-23 21:01:12 +0200

    WpfView project window is displayed as the program starts

--- WpfView/UI.xaml.cs
+++ WpfView/UI.xaml.cs
@@ -18,9 +18,9 @@ namespace WpfView
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class Ui : Window
     {
-        public UserControl1()
+        public Ui()
         {
             InitializeComponent();
         }
